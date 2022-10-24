b8342dac6b546130d737d1155b32f587947c79a4
Jefferson <jefferson-67@live.com>
2022-10-24 20:43:52 -0300

    It adds some details

--- Dapper.menu.Persistence/Entities/Pax.cs
+++ Dapper.menu.Persistence/Entities/Pax.cs
@@ -3,8 +3,20 @@
     [Table("Pax")]
     public class Pax : BaseEntity
     {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
+        public string? Name { get; private set; }
+        public string? Email { get; private set; }
+        public string? PhoneNumber { get; private set; }
+
+        public Pax(string name, string email, string phoneNumber)
+        {
+            Name = name;
+            Email = email;
+            PhoneNumber = phoneNumber;
+        }
+
+        private Pax() { }
+
+        void UpdateEmail(string email) =>  Email = email;
+        
     }
 }
